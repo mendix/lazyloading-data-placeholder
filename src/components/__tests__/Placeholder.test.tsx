50ff5c9f14c692608bb871b0a5300f4317b67aa4
meisam.mahdian <meisam.mahdian@mendix.com>
2019-10-15 16:35:56 +0200

    Update tests as react-test-renderer

--- src/components/__tests__/Placeholder.test.tsx
+++ src/components/__tests__/Placeholder.test.tsx
@@ -1,23 +1,43 @@
+import React from 'react';
+import { ReactTestInstance, create } from 'react-test-renderer';
+
+import parseStyle from '../../utils/parseStyle';
 import { Placeholder, PlaceholderProps } from '../Placeholder';
 
-import React from 'react';
-import parseStyle from '../../utils/parseStyle';
-import { render } from '@testing-library/react';
+const hasClass = (className: string, expectedClass: string) => {
+  return className.indexOf(expectedClass) > -1;
+};
+
+const itemsCount = 2;
+const svgText = `<svg style="width:100%; height:100%;">
+<rect x="10%" y="10%" rx="4" ry="4" width="45%" height="10"></rect>
+<rect x="60%" y="10%" rx="4" ry="4" width="35%" height="10"></rect>
+</svg>`;
 
 describe('Placeholder Component Test Unit', () => {
   it('should render correctly', () => {
     const props: PlaceholderProps = {
-      skeletonSVG: `<svg style="width:100%; height:100%;">
-        <rect x="10%" y="10%" rx="4" ry="4" width="45%" height="10"></rect>
-        <rect x="60%" y="10%" rx="4" ry="4" width="35%" height="10"></rect>
-      </svg>`,
-      repeatCount: 2,
+      skeletonSVG: svgText,
+      repeatCount: itemsCount,
       containerClassName: 'myContainerClass',
       containerStyle: parseStyle('width:400px;height:300px;'),
       itemClassName: 'myItemClass',
       itemStyle: parseStyle('height:20px;'),
     };
-    const { container } = render(<Placeholder {...props} />);
-    expect(container.firstChild).toMatchSnapshot();
+
+    const testRenderer = create(<Placeholder {...props} />);
+    const divItems = testRenderer.root.findAllByType('div');
+
+    const myContainers = divItems.filter(i =>
+      hasClass(i.props.className, 'myContainerClass')
+    );
+    expect(myContainers.length).toBe(1);
+    expect(myContainers[0].props.style.height).toBe('300px');
+
+    const myItems = divItems.filter(i =>
+      hasClass(i.props.className, 'myItemClass')
+    );
+    expect(myItems.length).toBe(2);
+    expect(myItems[0].props.style.height).toBe('20px');
   });
 });
