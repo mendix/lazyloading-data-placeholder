f6c04b1122024d49e6d12588246840ab3e3a08c9
meisam.mahdian <meisam.mahdian@mendix.com>
2019-10-16 09:49:38 +0200

    Improve test

--- src/components/__tests__/Placeholder.test.tsx
+++ src/components/__tests__/Placeholder.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { ReactTestInstance, create } from 'react-test-renderer';
+import { create } from 'react-test-renderer';
 
 import parseStyle from '../../utils/parseStyle';
+import { Ghost } from '../Ghost';
 import { Placeholder, PlaceholderProps } from '../Placeholder';
 
 const hasClass = (className: string, expectedClass: string) => {
@@ -13,31 +14,42 @@ const svgText = `<svg style="width:100%; height:100%;">
 <rect x="10%" y="10%" rx="4" ry="4" width="45%" height="10"></rect>
 <rect x="60%" y="10%" rx="4" ry="4" width="35%" height="10"></rect>
 </svg>`;
+const containerClassName = 'myContainerClass';
+const containerStyle = 'width:400px;height:300px;';
+const itemClassName = 'myItemClass';
+const itemStyle = 'height:20px';
+
+const props: PlaceholderProps = {
+  skeletonSVG: svgText,
+  repeatCount: itemsCount,
+  containerClassName: containerClassName,
+  containerStyle: parseStyle(containerStyle),
+  itemClassName: itemClassName,
+  itemStyle: parseStyle(itemStyle),
+};
 
 describe('Placeholder Component Test Unit', () => {
-  it('should render correctly', () => {
-    const props: PlaceholderProps = {
-      skeletonSVG: svgText,
-      repeatCount: itemsCount,
-      containerClassName: 'myContainerClass',
-      containerStyle: parseStyle('width:400px;height:300px;'),
-      itemClassName: 'myItemClass',
-      itemStyle: parseStyle('height:20px;'),
-    };
+  const testRenderer = create(<Placeholder {...props} />);
+  const divItems = testRenderer.root.findAllByType('div');
 
-    const testRenderer = create(<Placeholder {...props} />);
-    const divItems = testRenderer.root.findAllByType('div');
-
+  it('should render one container', () => {
     const myContainers = divItems.filter(i =>
-      hasClass(i.props.className, 'myContainerClass')
+      hasClass(i.props.className, containerClassName)
     );
     expect(myContainers.length).toBe(1);
     expect(myContainers[0].props.style.height).toBe('300px');
+  });
 
+  it('should render ghosts inside container', () => {
     const myItems = divItems.filter(i =>
-      hasClass(i.props.className, 'myItemClass')
+      hasClass(i.props.className, itemClassName)
     );
     expect(myItems.length).toBe(2);
     expect(myItems[0].props.style.height).toBe('20px');
+  });
+
+  it('should pass SVG to the ghost', () => {
+    const ghostItems = testRenderer.root.findAllByType(Ghost);
+    expect(ghostItems[0].props.skeletonSVG).toBe(svgText);
   });
 });
